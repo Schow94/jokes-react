1606d97126b369a40d84f367a4940c5fca504300
Schow94 <Schow94@gmail.com>
2019-08-08 07:43:22 -0700

    Updates

---
+++ precache-manifest.77f55093077e8b71101005c579e76ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b371bfa72873449210a4e986e2a80cd",
+    "url": "/jokes-react/index.html"
+  },
+  {
+    "revision": "9e4c7fd2afbfd653bc62",
+    "url": "/jokes-react/static/css/main.affddd92.chunk.css"
+  },
+  {
+    "revision": "06a1d4798b01652d19f7",
+    "url": "/jokes-react/static/js/2.d186c5e5.chunk.js"
+  },
+  {
+    "revision": "9e4c7fd2afbfd653bc62",
+    "url": "/jokes-react/static/js/main.e5339635.chunk.js"
+  },
+  {
+    "revision": "dd48708ffb2c6a9717a0",
+    "url": "/jokes-react/static/js/runtime~main.0070fafe.js"
+  }
+]);
